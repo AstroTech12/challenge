ff08cb4a3799af1b55eee67dfd85316ed1ce4f23
AstroTech12 <48364641+AstroTech12@users.noreply.github.com>
2021-02-27 15:29:45 +0200

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -1,15 +1,29 @@
-# Ask for cities, exp, tendency
-# Validate the data
-def ask_for_data():
-    cities = input("How many cities do you want to input from 1-3 ")
-    what_cities = input("What cities do you want to input ")
-    exp = input("How much c-19 exposure is there in " + what_cities)
-    tendency = input("Is the exposure rising, stable, or falling ")
-    print(cities, what_cities, exp, tendency)
-    print("Your amount of cities is " + cities, "Your cities are " +  what_cities, "The exposure is " + exp, "The tendency is " + tendency)
+import sorting
+# Ask for cities
+def main_function():
+	input("Choose 3 Cities, type Yes to continue ")
+	city1 = input("What is your first city? ")
+	city2 = input("What is your second city? ")
+	city3 = input("What is your third city? ")
+	print("Your first city is " + city1 + " your second city is " + city2 + " your third city is " + city3)
+
+	#ask for exposure
+	exposure1 = input("How many c-19 cases are there in " + city1 + ". ")
+	exposure2 = input("How many c-19 cases are there in " + city2 + ". ")
+	exposure3 = input("How many c-19 cases are there in " + city3 + ". ")
+	print("There are " + exposure1 + " cases in " + city1 + ", " + exposure2 + " cases in " + city2 + " and there are " + exposure3 + " cases in " + city3 + ".")
+
+	#ask for tendency 
+	tendency1 = input("Are the cases going higher, lower or staying stable in " + city1)
+	tendency2 = input("Are the cases going higher, lower or staying stable in " + city2)
+	tendency3 = input("Are the cases going higher, lower or staying stable in " + city3)
+	print("The cases are " + tendency1 + " in " + city1 + " The cases are " + tendency2 + " in " + city2 + " The cases are " + tendency3 + " in " + city3)
+
+	#sort data
+	covid_list = [exposure1, exposure2, exposure3]
+	covid_list.sort(reverse = True)
+	print (covid_list)
 
 
-ask_for_data()
 
-
-# sort the data
+main_function()
